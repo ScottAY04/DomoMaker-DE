384e1bf7b136a6c302cf91600f779e1268cee7fc
ScottAY04 <auyeungscott@gmail.com>
2026-04-10 17:21:25 -0400

    finished domo maker d

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    entry: './client/client.js',
-    mode: 'production',
-    watchOptions: {
-        aggregateTimeout: 200,
+    entry: {
+        app: './client/maker.jsx',
+        login: './client/login.jsx',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
     },
     output: {
         path: path.resolve(__dirname, 'hosted'),
-        filename: 'bundle.js',
-    },
+        filename: '[name]Bundle.js',
+    }
 };
